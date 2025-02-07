703bfe0d814c6321107079d057f1c27037042016
unknown <jezrreld@gmail.com>
2025-02-06 21:21:24 -0300

    Adicionado seletores

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,27 +1,36 @@
 describe('Orange Teste', () => {
-  it.skip('Login teste', () => {
+  
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButtom: ".oxd-button",
+    selectiontitletopbar: ".oxd-text--h6",
+
+  }
+
+  it('Login teste', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Admin')
-    cy.get("[name='password']").type('admin123')
-    cy.get('.oxd-button').click()
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.loginButtom).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get('.oxd-text--h6').contains('Dashboard')
+    cy.get(selectorsList.selectiontitletopbar).contains('Dashboard')
   })
 
   it('User errado', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('ADEMI')
-    cy.get("[name='password']").type('admin123')
-    cy.get('.oxd-button').click()
+    cy.get(selectorsList.usernameField).type('ADEMI')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.loginButtom).click()
     cy.location('pathname').should('equal', '/web/index.php/auth/login')
     cy.get('.oxd-alert')
   })
 
   it('Senha errado', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Admin')
-    cy.get("[name='password']").type('ADEMI')
-    cy.get('.oxd-button').click()
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('ADEMI')
+    cy.get(selectorsList.loginButtom).click()
     cy.location('pathname').should('equal', '/web/index.php/auth/login')
     cy.get('.oxd-alert')
   })
